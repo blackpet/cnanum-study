d0cb57011dec44d159a5b8ca946bb91a3ee0a5e3
kangdaeguk <71801790+kangdaeguk@users.noreply.github.com>
2020-11-13 01:12:40 +0900

    동적 binding

--- study06/vending-machine.js
+++ study06/vending-machine.js
@@ -17,7 +17,10 @@ var drink_lis; // 음료 item LI array
 var drink_btns; // 음료 선택 버튼 array
 var drink_qtys; // 수량 array
 function init() {
-    drink_lis = document.querySelectorAll('li');
+    // array data를 화면에 동적으로 출력하자!
+    renderDrinks();
+
+    drink_lis = document.querySelectorAll('ul#drink-items li');
     drink_btns = document.querySelectorAll('li button');
     drink_qtys = document.querySelectorAll('.qty');
     // 시작 시에는 버튼 비활성화!!
@@ -25,6 +28,27 @@ function init() {
         var btn = drink_btns[i];
         btn.disabled = true;
     }
+}
+
+// array data를 화면에 동적으로 출력하자!
+function renderDrinks() {
+    var drink_li_html = '';
+    // array data 만큼 돌려라~
+    for(var i = 0; i < drinks.length; i++) {
+        var d = drinks[i];
+        drink_li_html = drink_li_html + `
+        <li>
+          <div class="item">
+            <!-- 음료마다  -->
+            <h3>${d.name}</h3>
+            <div class="price">${d.price.toLocaleString()}원</div>
+            <div class="qty">수량 : ${d.qty.toLocaleString()}</div>
+            <button class="buy" onclick="buyDrink(${i})">선택</button>
+            <button class="remove" onclick="removeDrink2(${i})">삭제</button>
+          </div>
+        </li>`;
+    }
+    document.querySelector('ul#drink-items').innerHTML = drink_li_html;
 }
 
 // 돈넣고~ 음료먹기~
@@ -99,6 +123,73 @@ function buyDrink(idx) {
         document.getElementById('remain-money').innerText = `거스름돈은 ${myMoney}원 입니다.`;
     } else {
         document.getElementById('remain-money').innerText = '';
+    }
+}
+
+// 자판기에 음료 품목 추가!
+function addDrink() {
+    // 신규 음료 forms에서 음료 정보를 가져오자!
+    var frm = document.newDrink;
+    var newDrinkName = frm.drink_name.value;
+    var newDrinkPrice = frm.drink_price.value;
+    var newDrinkQty = frm.drink_qty.value;
+
+
+    drinks.push({name: newDrinkName, price: parseInt(newDrinkPrice), qty: parseInt(newDrinkQty)})
+    
+    // 신규 음료 추가 했으니 페이지 전체를 초기화 해주자!
+    // 입력 항목 모두 초기화!
+    frm.drink_name.value = '';
+    frm.drink_price.value = '';
+    frm.drink_qty.value = '';
+    document.getElementById('myMoney').value = '';
+    // 프로그램 전체를 처음부터 다시 시작!
+    init();
+}
+
+function removeDrink1() {
+    var drinkName = document.removeDrink.drink_name.value;
+
+    for(var i = 0; i < drinks.length; i++) {
+        if(drinks[i].name === drinkName) {
+            drinks.splice(i, 1);
+            break;
+        }
+    }
+    
+    document.getElementById('myMoney').value = '';
+    // 프로그램 전체를 처음부터 다시 시작!
+    init();
+}
+
+function removeDrink2(idx) {
+    console.log('idxidx', idx);
+    drinks.splice(idx, 1);
+    
+    document.getElementById('myMoney').value = '';
+    // 프로그램 전체를 처음부터 다시 시작!
+    init();
+}
+
+function removeDrink3() {
+    for(var i = 0; i < drink_lis.length; i++) {
+        var newDeleteBtn = document.createElement('button');
+        newDeleteBtn.addEventListener('click', removeDrink4);
+        newDeleteBtn.innerText = '삭제3';
+        newDeleteBtn.setAttribute('idx', i);
+        drink_lis[i].appendChild(newDeleteBtn);
+    }
+}
+
+function removeDrink4(e) {
+    console.log('i', e.target.attributes['idx'].nodeValue);
+    removeDrink2(parseInt(e.target.attributes['idx'].nodeValue));
+}
+
+function removeDrink5() {
+    var btns = document.querySelectorAll('#drink-items button.remove');
+    for(var i = 0; i < btns.length; i++) {
+        btns[i].disabled = false;
     }
 }
 
