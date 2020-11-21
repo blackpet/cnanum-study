50d6dcc21d6150df23b38fe9b817bed830a7be34
kangdaeguk <71801790+kangdaeguk@users.noreply.github.com>
2020-11-21 23:25:17 +0900

    동적으로 출력하기 함수 통합하기

---
+++ master/function.js
@@ -0,0 +1,20 @@
+var a = 1;
+var b = 2;
+
+function add(no1, no2) {
+    return no1 + no2;
+}
+
+console.log(add(a, b));
+
+var ccc = 6;
+var myno123 = 12;
+
+add(ccc, myno123);
+
+
+function 세차하자(a, b) {
+    // ${a} 열심히 세차를 하자
+
+    // 끝났으면 ${b}로 ${a}를 옮겨놓자
+}
--- study07/javascript.js
+++ study07/javascript.js
@@ -14,40 +14,41 @@ var c_lists = [
 var tListOl; // t_list의 할 일
 var cListOl; // 완료 한 일
 
-tListOl = document.querySelectorAll('.w_list'); // 할 일 리스트 불러오기
-cListOl = document.querySelectorAll('.c_list'); // 할 일 리스트 불러오기
+tListOl = document.querySelector('.w_list'); // 할 일 리스트 불러오기
+cListOl = document.querySelector('.c_list'); // 할 일 리스트 불러오기
 
 // 프로그램을 처음부터 다시 시작
 function init() {
-    rendertList(t_lists, tListOl); // 할 일 리스트 출력
-    rendercList(); // 완료한 일 리스트 출력 
+    renderList(t_lists, tListOl); // 할 일 리스트 출력
+    renderList(c_lists, cListOl); // 완료한 일 리스트 출력 
 }
 
 // tList 동적으로 출력
-function rendertList(lists, ListOl) {
+function renderList(lists, ListOl) {
     var wwork_li_html = '';
     for(var i = 0; i < lists.length; i++){
         wwork_li_html = wwork_li_html + `
         <li>
-        <div class="works">${(left)[i].todo}</div>
+        <div class="works">${lists[i].todo}</div>
         <button onclick="completeBtn(${i})">완료</button>
         </li>`;
+        console.log(wwork_li_html)
     }
-    document.querySelector('.w_list ol').innerHTML = wwork_li_html;
+    ListOl.innerHTML = wwork_li_html;
 }
 
-// cList 동적으로 출력
-function rendercList() {
-    var cwork_li_html = '';
-    for (var i = 0; i < c_lists.length; i++){
-        var c = c_lists[i];
-        cwork_li_html = cwork_li_html + `
-        <li>
-        <div class="works">${c.todo}</div>
-        </li>`;
-    }
-    document.querySelector('.c_list ol').innerHTML = cwork_li_html;
-}
+// // cList 동적으로 출력
+// function rendercList() {
+//     var cwork_li_html = '';
+//     for (var i = 0; i < c_lists.length; i++){
+//         var c = c_lists[i];
+//         cwork_li_html = cwork_li_html + `
+//         <li>
+//         <div class="works">${c.todo}</div>
+//         </li>`;
+//     }
+//     document.querySelector('.c_list ol').innerHTML = cwork_li_html;
+// }
 
 // 해야할 일을 tList에 추가
 function addWorkBtn() {
@@ -70,5 +71,4 @@ function completeBtn(idx) {
 }
 
 
-rendertList();
-rendercList();
+init();
