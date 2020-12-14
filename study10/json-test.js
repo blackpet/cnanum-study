c015610f6b542cc116ebc117c362337cba786f59
kangdaeguk <71801790+kangdaeguk@users.noreply.github.com>
2020-12-14 23:57:13 +0900

    미션!

---
+++ study10/json-test.js
@@ -0,0 +1,574 @@
+var obj = [
+    {
+      "_id": "5fd77b9d9f2c820656a20306",
+      "index": 0,
+      "guid": "63cd1c50-638a-40c9-80f6-ea7aaa7e9237",
+      "isActive": true,
+      "balance": "$1,364.15",
+      "picture": "http://placehold.it/32x32",
+      "age": 23,
+      "eyeColor": "brown",
+      "name": {
+        "first": "Marylou",
+        "last": "Stevenson"
+      },
+      "company": "RODEOCEAN",
+      "email": "marylou.stevenson@rodeocean.com",
+      "phone": "+1 (879) 599-2168",
+      "address": "584 Colonial Road, Wheatfields, Mississippi, 760",
+      "about": "Voluptate aliquip eiusmod adipisicing sit amet aliquip. Qui consectetur ex velit eu aute non nostrud quis ad sit. Anim amet officia in consectetur ad sit aliqua.",
+      "registered": "Tuesday, January 22, 2019 2:21 AM",
+      "latitude": "53.612383",
+      "longitude": "-132.262128",
+      "tags": [
+        "ullamco",
+        "cillum",
+        "mollit",
+        "reprehenderit",
+        "minim"
+      ],
+      "range": [
+        0,
+        1,
+        2,
+        3,
+        4,
+        5,
+        6,
+        7,
+        8,
+        9
+      ],
+      "friends": [
+        {
+          "id": 0,
+          "name": "Fry Kane"
+        },
+        {
+          "id": 1,
+          "name": "Simone Dawson"
+        },
+        {
+          "id": 2,
+          "name": "Trina Santana"
+        }
+      ],
+      "greeting": "Hello, Marylou! You have 10 unread messages.",
+      "favoriteFruit": "strawberry"
+    },
+    {
+      "_id": "5fd77b9d5936a001127a8f04",
+      "index": 1,
+      "guid": "95fe2a19-48c1-4db0-9b59-b1703103a62a",
+      "isActive": true,
+      "balance": "$1,458.87",
+      "picture": "http://placehold.it/32x32",
+      "age": 40,
+      "eyeColor": "brown",
+      "name": {
+        "first": "Compton",
+        "last": "Mcguire"
+      },
+      "company": "OCTOCORE",
+      "email": "compton.mcguire@octocore.tv",
+      "phone": "+1 (869) 547-2447",
+      "address": "788 Bragg Court, Cataract, California, 3119",
+      "about": "Cillum commodo cupidatat dolor deserunt pariatur eiusmod ea ex. Magna eiusmod ullamco nostrud officia dolor minim minim laborum aute aliquip deserunt cillum. Laborum cillum cupidatat dolore cupidatat pariatur ullamco est in exercitation qui consequat nulla culpa id. Pariatur consequat voluptate ut dolore reprehenderit elit veniam aliqua velit officia deserunt consequat anim consectetur. Eiusmod labore reprehenderit nisi duis et excepteur. Nisi nisi voluptate aliqua enim Lorem consequat ad. Sint laboris do consectetur non consectetur tempor aute id et labore excepteur aliquip magna.",
+      "registered": "Wednesday, June 3, 2015 10:00 PM",
+      "latitude": "80.520934",
+      "longitude": "177.663935",
+      "tags": [
+        "incididunt",
+        "eiusmod",
+        "non",
+        "magna",
+        "sit"
+      ],
+      "range": [
+        0,
+        1,
+        2,
+        3,
+        4,
+        5,
+        6,
+        7,
+        8,
+        9
+      ],
+      "friends": [
+        {
+          "id": 0,
+          "name": "Ophelia Tucker"
+        },
+        {
+          "id": 1,
+          "name": "Langley Chambers"
+        },
+        {
+          "id": 2,
+          "name": "Yvonne Dale"
+        }
+      ],
+      "greeting": "Hello, Compton! You have 7 unread messages.",
+      "favoriteFruit": "strawberry"
+    },
+    {
+      "_id": "5fd77b9da9c162affb08a49a",
+      "index": 2,
+      "guid": "ddb69a21-2e18-4596-8825-d46919c24420",
+      "isActive": true,
+      "balance": "$2,226.54",
+      "picture": "http://placehold.it/32x32",
+      "age": 30,
+      "eyeColor": "green",
+      "name": {
+        "first": "Callie",
+        "last": "Pope"
+      },
+      "company": "KONNECT",
+      "email": "callie.pope@konnect.io",
+      "phone": "+1 (850) 596-2455",
+      "address": "666 Conselyea Street, Wikieup, Georgia, 4478",
+      "about": "Amet exercitation consectetur irure tempor Lorem velit amet et est in tempor proident. Culpa Lorem dolore tempor cupidatat sit id ut reprehenderit qui. Ullamco mollit pariatur labore ad aliqua sit cillum quis qui. Laboris nulla incididunt voluptate tempor sint reprehenderit est anim sint duis tempor nulla in.",
+      "registered": "Wednesday, January 24, 2018 3:56 AM",
+      "latitude": "-52.407079",
+      "longitude": "21.028731",
+      "tags": [
+        "cillum",
+        "aliqua",
+        "magna",
+        "exercitation",
+        "voluptate"
+      ],
+      "range": [
+        0,
+        1,
+        2,
+        3,
+        4,
+        5,
+        6,
+        7,
+        8,
+        9
+      ],
+      "friends": [
+        {
+          "id": 0,
+          "name": "Lupe Avila"
+        },
+        {
+          "id": 1,
+          "name": "Barrett Best"
+        },
+        {
+          "id": 2,
+          "name": "Odom Beck"
+        }
+      ],
+      "greeting": "Hello, Callie! You have 7 unread messages.",
+      "favoriteFruit": "strawberry"
+    },
+    {
+      "_id": "5fd77b9dbfc2e5e340c7dec4",
+      "index": 3,
+      "guid": "f412cba5-2e23-4154-81ad-67e1c65d234f",
+      "isActive": true,
+      "balance": "$1,416.79",
+      "picture": "http://placehold.it/32x32",
+      "age": 26,
+      "eyeColor": "green",
+      "name": {
+        "first": "Deann",
+        "last": "Arnold"
+      },
+      "company": "ORBOID",
+      "email": "deann.arnold@orboid.net",
+      "phone": "+1 (939) 515-2626",
+      "address": "366 Rockaway Avenue, Riceville, Ohio, 9655",
+      "about": "Occaecat qui sunt laboris velit pariatur Lorem. Incididunt eiusmod elit ut ex enim ex velit ea dolore anim duis velit proident incididunt. Proident excepteur ex do tempor elit duis officia eiusmod sint cillum aute fugiat. Voluptate reprehenderit magna ullamco aliquip incididunt qui culpa proident. Lorem cupidatat esse amet laboris nisi velit labore pariatur nostrud sit deserunt duis.",
+      "registered": "Wednesday, April 9, 2014 6:27 AM",
+      "latitude": "-8.564459",
+      "longitude": "168.8792",
+      "tags": [
+        "tempor",
+        "irure",
+        "amet",
+        "ipsum",
+        "incididunt"
+      ],
+      "range": [
+        0,
+        1,
+        2,
+        3,
+        4,
+        5,
+        6,
+        7,
+        8,
+        9
+      ],
+      "friends": [
+        {
+          "id": 0,
+          "name": "Acosta Beard"
+        },
+        {
+          "id": 1,
+          "name": "Sims Blair"
+        },
+        {
+          "id": 2,
+          "name": "Dickerson Patterson"
+        }
+      ],
+      "greeting": "Hello, Deann! You have 9 unread messages.",
+      "favoriteFruit": "apple"
+    },
+    {
+      "_id": "5fd77b9d0b91856f760f5b1f",
+      "index": 4,
+      "guid": "209548bd-674c-4da3-bb0d-8cfe4b217297",
+      "isActive": false,
+      "balance": "$2,327.73",
+      "picture": "http://placehold.it/32x32",
+      "age": 35,
+      "eyeColor": "brown",
+      "name": {
+        "first": "Brenda",
+        "last": "Mosley"
+      },
+      "company": "EQUICOM",
+      "email": "brenda.mosley@equicom.info",
+      "phone": "+1 (990) 403-2274",
+      "address": "919 Monitor Street, Faywood, Rhode Island, 3758",
+      "about": "Culpa amet consectetur ad laborum ea nulla exercitation incididunt sint excepteur dolore nisi sint anim. Nulla consectetur cillum pariatur ea. Veniam dolore minim cupidatat cupidatat sit reprehenderit. Occaecat pariatur quis dolore amet ad nostrud enim commodo irure labore. Sint pariatur do non est consectetur nisi consectetur aliqua duis excepteur sunt enim. Ullamco est consectetur minim nisi aliquip veniam sunt duis voluptate. Commodo labore irure nostrud occaecat ullamco cillum culpa sit magna labore id dolor.",
+      "registered": "Tuesday, October 27, 2015 4:39 PM",
+      "latitude": "6.070267",
+      "longitude": "58.012292",
+      "tags": [
+        "enim",
+        "qui",
+        "veniam",
+        "commodo",
+        "consequat"
+      ],
+      "range": [
+        0,
+        1,
+        2,
+        3,
+        4,
+        5,
+        6,
+        7,
+        8,
+        9
+      ],
+      "friends": [
+        {
+          "id": 0,
+          "name": "Mayra Lindsay"
+        },
+        {
+          "id": 1,
+          "name": "Adeline Peck"
+        },
+        {
+          "id": 2,
+          "name": "Rosario Patrick"
+        }
+      ],
+      "greeting": "Hello, Brenda! You have 7 unread messages.",
+      "favoriteFruit": "apple"
+    },
+    {
+      "_id": "5fd77b9db5909932ceb7e08a",
+      "index": 5,
+      "guid": "347c2341-18a4-4f0c-8810-1123825d7df5",
+      "isActive": false,
+      "balance": "$3,886.29",
+      "picture": "http://placehold.it/32x32",
+      "age": 37,
+      "eyeColor": "green",
+      "name": {
+        "first": "Fields",
+        "last": "Cooke"
+      },
+      "company": "PAPRICUT",
+      "email": "fields.cooke@papricut.org",
+      "phone": "+1 (930) 488-2520",
+      "address": "696 Fair Street, Jeff, American Samoa, 7864",
+      "about": "Consectetur tempor eiusmod mollit consequat aliqua proident elit eiusmod et esse cupidatat. Pariatur culpa ipsum do sint do id deserunt consequat ipsum adipisicing. Culpa sit ex dolore ullamco qui nisi. Est excepteur ullamco culpa Lorem nulla occaecat veniam pariatur voluptate.",
+      "registered": "Friday, August 21, 2015 4:06 PM",
+      "latitude": "61.797503",
+      "longitude": "-62.543505",
+      "tags": [
+        "Lorem",
+        "incididunt",
+        "excepteur",
+        "fugiat",
+        "irure"
+      ],
+      "range": [
+        0,
+        1,
+        2,
+        3,
+        4,
+        5,
+        6,
+        7,
+        8,
+        9
+      ],
+      "friends": [
+        {
+          "id": 0,
+          "name": "Giles Dotson"
+        },
+        {
+          "id": 1,
+          "name": "Bernard Holcomb"
+        },
+        {
+          "id": 2,
+          "name": "Morton Henson"
+        }
+      ],
+      "greeting": "Hello, Fields! You have 10 unread messages.",
+      "favoriteFruit": "banana"
+    },
+    {
+      "_id": "5fd77b9d12e1f97d094cbd4d",
+      "index": 6,
+      "guid": "16c90928-d5b3-44fd-bd6e-a1cb5a1b2bbd",
+      "isActive": false,
+      "balance": "$3,240.03",
+      "picture": "http://placehold.it/32x32",
+      "age": 24,
+      "eyeColor": "brown",
+      "name": {
+        "first": "Deana",
+        "last": "Pollard"
+      },
+      "company": "EXTRO",
+      "email": "deana.pollard@extro.co.uk",
+      "phone": "+1 (843) 458-3244",
+      "address": "237 Dean Street, Fostoria, New Jersey, 4170",
+      "about": "Esse dolor amet in veniam qui irure nostrud incididunt nulla sunt sint aliquip. Aliqua ad irure aliqua commodo in labore. Sit nisi consectetur adipisicing do.",
+      "registered": "Friday, June 16, 2017 3:21 PM",
+      "latitude": "66.539477",
+      "longitude": "29.268566",
+      "tags": [
+        "irure",
+        "magna",
+        "do",
+        "quis",
+        "magna"
+      ],
+      "range": [
+        0,
+        1,
+        2,
+        3,
+        4,
+        5,
+        6,
+        7,
+        8,
+        9
+      ],
+      "friends": [
+        {
+          "id": 0,
+          "name": "Cherie Collins"
+        },
+        {
+          "id": 1,
+          "name": "Frazier Calderon"
+        },
+        {
+          "id": 2,
+          "name": "Roy Neal"
+        }
+      ],
+      "greeting": "Hello, Deana! You have 10 unread messages.",
+      "favoriteFruit": "banana"
+    },
+    {
+      "_id": "5fd77b9d6179c1ca53cb8b16",
+      "index": 7,
+      "guid": "f57f3e7d-8117-42f3-abdb-b08d67b581d9",
+      "isActive": false,
+      "balance": "$1,852.17",
+      "picture": "http://placehold.it/32x32",
+      "age": 38,
+      "eyeColor": "green",
+      "name": {
+        "first": "Dora",
+        "last": "Walls"
+      },
+      "company": "BISBA",
+      "email": "dora.walls@bisba.me",
+      "phone": "+1 (993) 521-2080",
+      "address": "326 Canda Avenue, Farmers, Alaska, 8730",
+      "about": "Dolore consequat in laborum minim est cillum enim sit velit. Nisi nisi cillum veniam tempor qui ad duis irure occaecat sunt magna ea laboris. Eu veniam nulla mollit elit duis.",
+      "registered": "Monday, May 23, 2016 1:58 AM",
+      "latitude": "-11.351476",
+      "longitude": "135.376426",
+      "tags": [
+        "elit",
+        "mollit",
+        "dolore",
+        "occaecat",
+        "minim"
+      ],
+      "range": [
+        0,
+        1,
+        2,
+        3,
+        4,
+        5,
+        6,
+        7,
+        8,
+        9
+      ],
+      "friends": [
+        {
+          "id": 0,
+          "name": "Powell Haynes"
+        },
+        {
+          "id": 1,
+          "name": "Espinoza Ellison"
+        },
+        {
+          "id": 2,
+          "name": "Castro West"
+        }
+      ],
+      "greeting": "Hello, Dora! You have 8 unread messages.",
+      "favoriteFruit": "apple"
+    },
+    {
+      "_id": "5fd77b9d5df994dee81fe0ca",
+      "index": 8,
+      "guid": "17f3a06c-de83-46c7-a6e4-e22b7a545d00",
+      "isActive": true,
+      "balance": "$2,008.66",
+      "picture": "http://placehold.it/32x32",
+      "age": 40,
+      "eyeColor": "blue",
+      "name": {
+        "first": "Rios",
+        "last": "Rivers"
+      },
+      "company": "EXOBLUE",
+      "email": "rios.rivers@exoblue.name",
+      "phone": "+1 (895) 593-2739",
+      "address": "268 Graham Avenue, Bowden, Maryland, 3047",
+      "about": "Consequat consectetur tempor laboris tempor adipisicing. Ad et non laboris exercitation incididunt magna eiusmod excepteur deserunt id occaecat laboris aute sunt. Sint voluptate laborum ipsum est do et quis nulla nostrud et. Ea excepteur minim irure elit duis officia aliqua. Laboris laboris non consectetur consectetur dolor do.",
+      "registered": "Wednesday, March 22, 2017 5:02 AM",
+      "latitude": "-3.859758",
+      "longitude": "-73.040124",
+      "tags": [
+        "dolor",
+        "occaecat",
+        "non",
+        "nulla",
+        "irure"
+      ],
+      "range": [
+        0,
+        1,
+        2,
+        3,
+        4,
+        5,
+        6,
+        7,
+        8,
+        9
+      ],
+      "friends": [
+        {
+          "id": 0,
+          "name": "Claudette Dalton"
+        },
+        {
+          "id": 1,
+          "name": "Combs Lindsey"
+        },
+        {
+          "id": 2,
+          "name": "Burnett Torres"
+        }
+      ],
+      "greeting": "Hello, Rios! You have 8 unread messages.",
+      "favoriteFruit": "apple"
+    },
+    {
+      "_id": "5fd77b9d46c2b92470a8a3d1",
+      "index": 9,
+      "guid": "015e9cf0-94ed-421f-b5ae-fc528188009b",
+      "isActive": false,
+      "balance": "$1,501.49",
+      "picture": "http://placehold.it/32x32",
+      "age": 31,
+      "eyeColor": "green",
+      "name": {
+        "first": "Hale",
+        "last": "Hogan"
+      },
+      "company": "OTHERSIDE",
+      "email": "hale.hogan@otherside.biz",
+      "phone": "+1 (910) 539-3745",
+      "address": "631 Oriental Court, Gilmore, Illinois, 1379",
+      "about": "Tempor duis ex qui ex duis sint dolor. Aliquip amet officia proident cillum minim esse velit. Cillum labore minim esse cupidatat sunt amet elit dolor. Aliquip eiusmod ullamco laboris tempor laborum excepteur aute consequat officia reprehenderit.",
+      "registered": "Tuesday, April 30, 2019 10:14 AM",
+      "latitude": "-34.786801",
+      "longitude": "-112.50139",
+      "tags": [
+        "aute",
+        "cillum",
+        "dolore",
+        "velit",
+        "consectetur"
+      ],
+      "range": [
+        0,
+        1,
+        2,
+        3,
+        4,
+        5,
+        6,
+        7,
+        8,
+        9
+      ],
+      "friends": [
+        {
+          "id": 0,
+          "name": "Allison Ochoa"
+        },
+        {
+          "id": 1,
+          "name": "Dianna Nelson"
+        },
+        {
+          "id": 2,
+          "name": "Berger Carr"
+        }
+      ],
+      "greeting": "Hello, Hale! You have 7 unread messages.",
+      "favoriteFruit": "apple"
+    }
+];
+
+console.log(obj);
